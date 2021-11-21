317bb9f0b4e774e6e4fbf7b12026b3f613a57599
Begad <64358501+begadsalah@users.noreply.github.com>
2021-11-21 15:59:01 +0200

    Updates

---
+++ precache-manifest.1df321cfe63873d6584519bcedad1257.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cba47dd02545974dd905068cfd447f3",
+    "url": "/MajdStore/index.html"
+  },
+  {
+    "revision": "96e87b0b22017f0c038b",
+    "url": "/MajdStore/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "f94b09ef8d3bee5c0057",
+    "url": "/MajdStore/static/css/main.1286b849.chunk.css"
+  },
+  {
+    "revision": "96e87b0b22017f0c038b",
+    "url": "/MajdStore/static/js/2.538292a9.chunk.js"
+  },
+  {
+    "revision": "67364e4f8a92ee3414e0588d01fa6997",
+    "url": "/MajdStore/static/js/2.538292a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94b09ef8d3bee5c0057",
+    "url": "/MajdStore/static/js/main.a29a4aaa.chunk.js"
+  },
+  {
+    "revision": "2c48a376a1b16154529f",
+    "url": "/MajdStore/static/js/runtime-main.dbf59b3f.js"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/MajdStore/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/MajdStore/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/MajdStore/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MajdStore/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/MajdStore/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/MajdStore/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MajdStore/static/media/slickd41d.ced611da.eot"
+  }
+]);
